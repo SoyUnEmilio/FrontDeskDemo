f4e2056e9e373c085e1ed085a1b3ccfe56da2404
helmy204 <hamadaelking204@hotmail.com>
2016-03-21 14:39:59 +0200

    Creating Items

---
+++ FrontDeskDemo/FrontDesk.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FrontDesk.Web.MvcApplication" Language="C#" %>
---
+++ FrontDeskDemo/FrontDesk.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FrontDesk.Web.Startup))]
+namespace FrontDesk.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
